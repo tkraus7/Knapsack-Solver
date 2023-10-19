f553020f487d043c535241b29cc734c58d167b38
Kraus, Tomáš <krausto8@fel.cvut.cz>
2023-10-19 18:33:30 +0200

    some command line argument parsing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World! " << std::endl;
+using namespace std;
+
+int main(int argc, char** argv) {
+    for (int i = 1; i < argc; i++) {
+        string arg(argv[i]);
+        if (arg == "-p" || arg == "--parallel") {
+            cout << "Running program with multiple threads" << endl;
+        } else if (arg == "-f") {
+
+        } else {
+            cerr << "ERROR: Unknown command line argument: " << arg << endl;
+            return 1;
+        }
+    }
     return 0;
 }
